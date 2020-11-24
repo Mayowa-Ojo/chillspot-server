a8e1b3c5406aa360662e199140519d2544ec2921
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-11-24 13:31:49 -0700

    :sparkles: [feat]: create server entry

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import app from "./app";
+
+import { config } from "./config/env.config";
+import { connectDB } from "./config/db.config";
+
+const main = async () => {
+   await connectDB();
+
+   app.listen(config.PORT);
+}
+
+main().then(() => {
+   console.log(`[INFO]: development sever started. Available at ==> http://lcoalhost:6000/api/v1`);
+});
