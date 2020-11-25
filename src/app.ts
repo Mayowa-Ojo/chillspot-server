9ef88916c05a32f246b0956807ff3d8fcf5785f5
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-11-25 00:43:07 -0700

    :ambulance: [fix]: error handler precedence

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,18 @@
-import Koa, { BaseContext, Context } from "koa";
+import Koa from "koa";
 import cors from "@koa/cors";
 import helmet from "koa-helmet";
 import winston from "winston";
-import bodyparser from "koa-bodyparser";
+import bodyParser from "koa-bodyparser";
 
 import { logger } from "./config/logger.config";
 import { v1Router } from "./routes";
-import { errorHandler, JwtError } from "./handlers/error.handler";
+import { errorHandler, JwtError, NotFoundError } from "./handlers/error.handler";
 
 const app = new Koa();
 
-app.use(errorHandler);
-
 app.use(JwtError);
+
+app.use(errorHandler);
 
 app.use(helmet());
 
@@ -20,8 +20,10 @@ app.use(cors());
 
 app.use(logger(winston));
 
-app.use(bodyparser());
+app.use(bodyParser());
 
 app.use(v1Router.routes()).use(v1Router.allowedMethods());
+
+app.use(NotFoundError);
 
 export default app;
