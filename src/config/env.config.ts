0bb71dec08de4f61bed6d5ac70b0badcdc6513a7
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-11-27 23:50:52 -0700

    :sparkles: [feat]: add config variables

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -10,5 +10,9 @@ export const config: IEnvConfig = {
    API_VERSION: process.env.API_VERSION,
    MONGO_URI: process.env.MONGO_URI,
    DB_NAME: process.env.DB_NAME,
-   JWT_SECRET: process.env.JWT_SECRET
+   JWT_SECRET: process.env.JWT_SECRET,
+   AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+   AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+   AWS_REGION: process.env.AWS_REGION,
+   AWS_S3_BUCKET: process.env.AWS_S3_BUCKET
 }
