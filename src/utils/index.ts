99242c6325371cf2976983687fff128bd2b5ab42
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-12-01 14:58:57 -0700

    :recycle: [refactor]: return correct image type

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,6 +3,7 @@ import bcrypt from "bcrypt";
 
 import { find } from "~database/repository/user.repository";
 import { config } from "~config/env.config";
+import type { IImage } from "~declarations/index";
 
 export const generateUsername = async (
    name: { firstname: string, lastname: string }
@@ -75,14 +76,14 @@ export const verifyAuthToken = (token: string): string | object => {
    }
 }
 
-export const generateProfileImage = (): string => {
-   const defaultAvatars = [
-      "https://chillspot-s3-bucket.s3.us-east-2.amazonaws.com/images/avatar-biking.png",
-      "https://chillspot-s3-bucket.s3.us-east-2.amazonaws.com/images/avatar-skiing.png",
-      "https://chillspot-s3-bucket.s3.us-east-2.amazonaws.com/images/avatar-skating.png",
-      "https://chillspot-s3-bucket.s3.us-east-2.amazonaws.com/images/avatar-hiking.png",
-      "https://chillspot-s3-bucket.s3.us-east-2.amazonaws.com/images/avatar-snowboarding.png",
-      "https://chillspot-s3-bucket.s3.us-east-2.amazonaws.com/images/avatar-snowmobile.png",
+export const generateProfileImage = (): IImage => {
+   const defaultAvatars: IImage[] = [
+      { url: "https://chillspot-s3-bucket.s3.us-east-2.amazonaws.com/images/avatar-biking.png", key: "images/avatar-biking.png" },
+      { url: "https://chillspot-s3-bucket.s3.us-east-2.amazonaws.com/images/avatar-skiing.png", key: "images/avatar-skiing.png" },
+      { url: "https://chillspot-s3-bucket.s3.us-east-2.amazonaws.com/images/avatar-skating.png", key: "images/avatar-skating.png" },
+      { url: "https://chillspot-s3-bucket.s3.us-east-2.amazonaws.com/images/avatar-hiking.png", key: "images/avatar-hiking.png" },
+      { url: "https://chillspot-s3-bucket.s3.us-east-2.amazonaws.com/images/avatar-snowboarding.png", key: "images/avatar-snowboarding.png" },
+      { url: "https://chillspot-s3-bucket.s3.us-east-2.amazonaws.com/images/avatar-snowmobile.png", key: "images/avatar-snowmobile.png" },
    ]
 
    const randomKey = Math.floor(Math.random()*6);
