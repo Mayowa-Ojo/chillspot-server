855cec8b62c777aa20f9a87e78a04665c952250e
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-12-01 14:59:54 -0700

    :sparkles: [feat]: add auth middleare to protected routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,6 +6,7 @@ import * as storyHandler from "~handlers/story.handler";
 import * as userHandler from "~handlers/user.handler";
 import * as commentHandler from "~handlers/comment.handler";
 import * as imageHandler from "~handlers/image.handler";
+import { requiresAuth } from "../middleware";
 
 const upload = multer({
    limits: {
@@ -31,42 +32,42 @@ const imageRouter = new Router({
 });
 
 // stories router
-storyRouter.get("/feed", storyHandler.getFeedForUser);
-storyRouter.get("/:id", storyHandler.getStory);
+storyRouter.get("/feed", requiresAuth(), storyHandler.getFeedForUser);
+storyRouter.get("/:id", requiresAuth(), storyHandler.getStory);
 storyRouter.get("/search", storyHandler.searchStories);
-storyRouter.patch("/:id/like", storyHandler.likeStory);
-storyRouter.patch("/:id/unlike", storyHandler.unlikeStory);
-storyRouter.patch("/:id/save", storyHandler.addStoryToCollection);
-storyRouter.post("/", storyHandler.createStory);
-storyRouter.del("/:id", storyHandler.deleteStory);
+storyRouter.patch("/:id/like", requiresAuth(), storyHandler.likeStory);
+storyRouter.patch("/:id/unlike", requiresAuth(), storyHandler.unlikeStory);
+storyRouter.patch("/:id/save", requiresAuth(), storyHandler.addStoryToCollection);
+storyRouter.post("/", requiresAuth(), storyHandler.createStory);
+storyRouter.del("/:id", requiresAuth(), storyHandler.deleteStory);
 
 // auth router
 authRouter.post("/login", authHandler.userLogin);
 authRouter.post("/signup", authHandler.userSignup);
 
 // users router
-userRouter.get("/:id", userHandler.getUserProfile);
-userRouter.get("/:id/followers", userHandler.getFollowersForUser);
-userRouter.get("/:id/following", userHandler.getFollowingForUser);
-userRouter.patch("/:id/follow", userHandler.followUser);
-userRouter.patch("/:id/unfollow", userHandler.unfollowUser);
-userRouter.get("/:id/liked-stories", userHandler.getLikedStories);
-userRouter.get("/:id/collection", userHandler.getCollectionForUser);
-userRouter.get("/:id/stories", userHandler.getStoriesByUser);
+userRouter.get("/:id", requiresAuth(), userHandler.getUserProfile);
+userRouter.get("/:id/followers", requiresAuth(), userHandler.getFollowersForUser);
+userRouter.get("/:id/following", requiresAuth(), userHandler.getFollowingForUser);
+userRouter.patch("/:id/follow", requiresAuth(), userHandler.followUser);
+userRouter.patch("/:id/unfollow", requiresAuth(), userHandler.unfollowUser);
+userRouter.get("/:id/liked-stories", requiresAuth(), userHandler.getLikedStories);
+userRouter.get("/:id/collection", requiresAuth(), userHandler.getCollectionForUser);
+userRouter.get("/:id/stories", requiresAuth(), userHandler.getStoriesByUser);
 
 // comments router
-commentRouter.get("/", commentHandler.getCommentsForStory);
-commentRouter.get("/:id", commentHandler.getComment);
-commentRouter.post("/", commentHandler.createComment);
-commentRouter.patch("/:id/like", commentHandler.likeComment);
-commentRouter.patch("/:id/unlike", commentHandler.unlikeComment);
-commentRouter.patch("/:id/dislike", commentHandler.dislikeComment);
-commentRouter.patch("/:id/undo-dislike", commentHandler.undoDislikeComment);
-commentRouter.del("/:id", commentHandler.deleteComment);
+commentRouter.get("/", requiresAuth(), commentHandler.getCommentsForStory);
+commentRouter.get("/:id", requiresAuth(), commentHandler.getComment);
+commentRouter.post("/", requiresAuth(), commentHandler.createComment);
+commentRouter.patch("/:id/like", requiresAuth(), commentHandler.likeComment);
+commentRouter.patch("/:id/unlike", requiresAuth(), commentHandler.unlikeComment);
+commentRouter.patch("/:id/dislike", requiresAuth(), commentHandler.dislikeComment);
+commentRouter.patch("/:id/undo-dislike", requiresAuth(), commentHandler.undoDislikeComment);
+commentRouter.del("/:id", requiresAuth(), commentHandler.deleteComment);
 
 // images router
-imageRouter.post("/", upload.single("image"), imageHandler.uploadFileToBucket);
-imageRouter.del("/", imageHandler.deleteObjectFromBucket);
+imageRouter.post("/", requiresAuth(), upload.single("image"), imageHandler.uploadFileToBucket);
+imageRouter.del("/", requiresAuth(), imageHandler.deleteObjectFromBucket);
 
 v1Router.use("/api/v1", storyRouter.routes(), storyRouter.allowedMethods());
 v1Router.use("/api/v1", authRouter.routes(), authRouter.allowedMethods());
