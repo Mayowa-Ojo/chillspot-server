3d680ba2677ce04ba0fe67a8ab7233c0ed9a624e
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-12-08 00:58:52 -0700

    :sparkles: [feat]: add new fields to comment entity

--- src/database/entity/comment.entity.ts
+++ src/database/entity/comment.entity.ts
@@ -14,6 +14,12 @@ export class Comment {
    @Prop({ default: 0 })
    public dislikes: number
 
+   @Prop({ default: 0 })
+   public likedBy: [mongoose.Types.ObjectId]
+
+   @Prop({ default: 0 })
+   public dislikedBy: [mongoose.Types.ObjectId]
+
    @Prop()
    public author!: mongoose.Types.ObjectId
 
