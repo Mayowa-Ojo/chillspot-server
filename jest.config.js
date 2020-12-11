4cff49d43d6176e1565c58f28d4c9a6fc7dfd154
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-12-11 00:31:52 -0700

    :wrench: [config]: setup jest for typescript

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+   preset: "ts-jest",
+   coveragePathIgnorePatterns: ["/node_modules/"],
+   testMatch: ["**/__tests__/*.+(ts|tsx|js)"],
+   moduleNameMapper: {
+      "~routes/(.*)": "<rootDir>/src/routes/$1",
+      "~utils/(.*)": "<rootDir>/src/utils/$1",
+      "~config/(.*)": "<rootDir>/src/config/$1",
+      "~database/(.*)": "<rootDir>/src/database/$1",
+      "~declarations/(.*)": "<rootDir>/src/declarations/$1",
+      "~middleware/(.*)": "<rootDir>/src/middleware/$1",
+      "~handlers/(.*)": "<rootDir>/src/handlers/$1",
+   },
+   testEnvironment: "node",
+};
